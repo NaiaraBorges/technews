aced003cfa38859ec97bdc8aa75f906c5a7f10f0
Naiara Borges <naiarazk@gmail.com>
2022-10-21 22:05:13 -0300

    Adicionando estilos globais

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,21 @@
-import Document, { Html, Main, Head, NextScript } from 'next/document'
+import Document, { Html, Main, Head, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-          <Head>
+        <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-          </Head>
-          <body>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
           <Main />
           <NextScript />
-          </body>
-        </Html>
-    )
+        </body>
+      </Html>
+    );
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Inicio | tech.news</title>
       </Head>
 
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 }
